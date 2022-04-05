2f021d7ac395b196a4c9cb7970e76617d07c0865
zahraa7 <zahraa.knass@hotmail.com>
2022-04-05 16:38:17 +0300

    done

--- src/reducer.js
+++ src/reducer.js
@@ -1,6 +1,6 @@
-let state = { count: 0 };
+let state
 
-function changeState(state, action) {
+function changeState(state= { count: 0 }, action) {
   switch (action.type) {
     case "counter/increment":
       return { count: state.count + 1 };
@@ -18,3 +18,5 @@ function render() {
   const app = document.querySelector("#app");
   app.textContent = state.count;
 }
+dispatch({ type: "@@INIT" });
+dispatch({ type: "beef" });
